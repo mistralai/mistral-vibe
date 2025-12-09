b187bad6e689690c0afac65867f735909a52718c
Dark Light <dark.light@users.noreply>
2025-12-09 21:46:38 +0100

    v1.0.5

--- vibe/core/__init__.py
+++ vibe/core/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
 __all__ = ["__version__", "run_programmatic"]
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 from vibe.core.programmatic import run_programmatic
